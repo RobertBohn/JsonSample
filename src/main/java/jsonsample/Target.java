cbd8574b32ddb734d3d9aa9c03f751b233117310
Robert Bohn <robertbohn@hotmail.com>
2014-09-23 15:21:19 -0700

    initial version

---
+++ src/main/java/jsonsample/Target.java
@@ -0,0 +1,19 @@
+package jsonsample;
+
+public class Target {
+    private String name;
+    private int age;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return name + " age " + age;
+    }
+}
---
+++ src/test/java/jsonsample/JsonSampleTest.java
@@ -0,0 +1,12 @@
+package jsonsample;
+
+import junit.framework.Assert;
+import org.junit.Test;
+
+public class JsonSampleTest {
+
+    @Test
+    public void testString() {
+        Assert.assertTrue(true);
+    }
+}
